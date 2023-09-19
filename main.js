d2c0823b5c81f94fb51e246c5832515b5260c4ac
CodeBoy124 <koen7maart@icloud.com>
2023-09-19 09:24:40 +0200

    Added pwa support (not yet offline)

--- main.js
+++ main.js
@@ -916,3 +916,14 @@ window.addEventListener("load", async () => {
     const resetScoreButton = document.getElementById("reset-score");
     resetScoreButton.addEventListener("click", resetScore);
 })
+
+/* PWA */
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+  })
+}
---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/main.js",
+    "/logo.png"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCoffee).then(cache => {
+            cache.addAll(assets).catch(err => console.error(err));
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
